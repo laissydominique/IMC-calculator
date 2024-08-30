2063959d04748dbc4493bba45e7f107c114009e2
Laissy <laissydominique@gmail.com>
2024-08-30 11:42:04 -0300

    atualizacao..

--- document.js
+++ document.js
@@ -1,34 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const result = document.querySelector(".result");
-
-  function advanceButton() {
-    const btnAvancar = document.querySelector(".btn-avancar");
-    const headerContent = document.querySelector(".header-content");
-    const calculator = document.querySelector(".calculator");
-
-    btnAvancar.addEventListener("click", () => {
-      headerContent.classList.add("hidden");
-      calculator.classList.remove("hidden");
-    });
-  }
-
-  function backButton() {
-    const btnBack = document.querySelector(".btn-back");
-    
-    btnBack.addEventListener("click", () => {
-      const headerContent = document.querySelector(".header-content");
-      const calculator = document.querySelector(".calculator");
-      headerContent.classList.remove("hidden");
-      calculator.classList.add("hidden");
-    });
-  }
-
   function getResult() {
-    const headerContent = document.querySelector(".header-content");
-    const calculator = document.querySelector(".calculator");
-    const btnCalculate = document.querySelector(".btn-calculate");
+    const btnCalculate = document.querySelector("#button-calc");
+
+    if (!btnCalculate) {
+      console.error("Botão de cálculo não encontrado!");
+      return;
+    }
 
     btnCalculate.addEventListener("click", () => {
+      event.preventDefault();
       const weightInput = document.querySelector(".weight-input");
       const heightInput = document.querySelector(".height-input");
 
@@ -43,40 +23,48 @@ document.addEventListener("DOMContentLoaded", () => {
           const height = parseFloat(heightInput.value);
           const calculo = (weight / (height * height)) * 10000;
           const resultado = calculo.toFixed(1);
-          const valorImc = document.querySelector('.p-valor-imc');
+          const valorImc = document.querySelector("#p-valor-imc");
+          const afterClick = document.querySelector("#after-click");
 
           if (resultado <= 18.5) {
-            valorImc.innerHTML = `<p>Seu IMC é de ${resultado}, Você está <span>abaixo </span> do peso ideal. É importante buscar a orientação de um profissional de saúde para avaliar sua nutrição e saúde geral.</p>`;
+            afterClick.innerHTML = `<p style="color: rgb(130, 213, 112); font-size: 1.3rem;margin-bottom: 1rem;">Seu IMC:</p>`;
+            valorImc.innerHTML = `<p style="color: white; font-size: 1rem" >Seu IMC é de ${resultado}, Você está <span style="color:rgb(130, 213, 112);font-weight: bold;" >abaixo </span> do peso ideal. É importante buscar a orientação de um profissional de saúde para avaliar sua nutrição e saúde geral.</p>`;
           } else if (resultado >= 18.6 && resultado <= 24.9) {
-            valorImc.innerHTML = `<p>O seu IMC é de ${resultado}. Você está com <span>peso normal </span>. Continue mantendo uma alimentação equilibrada e prática de atividades físicas.</p>`;
+            afterClick.innerHTML = `<p style="color: rgb(130, 213, 112); font-size: 1.3rem;margin-bottom: 1rem;">Seu IMC:</p>`;
+            valorImc.innerHTML = `<p style="color: white; font-size: 1rem">O seu IMC é de ${resultado}. Você está com <span style="color:rgb(130, 213, 112);font-weight: bold;" >peso normal. </span>Continue mantendo uma alimentação equilibrada e prática de atividades físicas.</p>`;
           } else if (resultado < 18.5) {
-            valorImc.innerHTML = `<p>O seu IMC é de ${resultado}. Você está com <span>baixo peso </span>. É recomendável procurar orientação médica para avaliar sua saúde e considerar ajustes na dieta e estilo de vida.</p>`;
+            afterClick.innerHTML = `<p style="color: rgb(130, 213, 112); font-size: 1.3rem;margin-bottom: 1rem;">Seu IMC:</p>`;
+            valorImc.innerHTML = `<pstyle="color: white; font-size: 1rem">O seu IMC é de ${resultado}. Você está com <span style="color:rgb(130, 213, 112);font-weight: bold;" >baixo peso. </span>É recomendável procurar orientação médica para avaliar sua saúde e considerar ajustes na dieta e estilo de vida.</pstyle=>`;
           } else if (resultado >= 18.5 && resultado <= 24.9) {
-            valorImc.innerHTML = `<p>O seu IMC é de ${resultado}. Você está com <span>peso normal </span>. Mantenha uma alimentação equilibrada e pratique exercícios regularmente para manter sua saúde em dia.</p>`;
+            afterClick.innerHTML = `<p style="color: rgb(130, 213, 112); font-size: 1.3rem;margin-bottom: 1rem;">Seu IMC:</p>`;
+            valorImc.innerHTML = `<pstyle="color: white; font-size: 1rem">O seu IMC é de ${resultado}. Você está com <span style="color:rgb(130, 213, 112);font-weight: bold;" >peso normal. </span> Mantenha uma alimentação equilibrada e pratique exercícios regularmente para manter sua saúde em dia.</pstyle=>`;
           } else if (resultado >= 25 && resultado <= 29.9) {
-            valorImc.innerHTML = `<p>O seu IMC é de ${resultado}. Você está com <span>sobrepeso </span>. Considerar mudanças na dieta e aumentar a atividade física pode ser benéfico. Consulte um profissional de saúde para orientação.</p>`;
+            afterClick.innerHTML = `<p style="color: rgb(130, 213, 112); font-size: 1.3rem;margin-bottom: 1rem;">Seu IMC:</p>`;
+            valorImc.innerHTML = `<pstyle="color: white; font-size: 1rem">O seu IMC é de ${resultado}. Você está com <span style="color:rgb(130, 213, 112);font-weight: bold;" >sobrepeso. </span>Considerar mudanças na dieta e aumentar a atividade física pode ser benéfico. Consulte um profissional de saúde para orientação.</pstyle=>`;
           } else if (resultado >= 30 && resultado <= 34.9) {
-            valorImc.innerHTML = `<p>O seu IMC é de ${resultado}. Você está com <span>obesidade grau I </span>. É importante procurar orientação médica para desenvolver um plano de tratamento que possa incluir ajustes na dieta e exercícios físicos.</p>`;
+            afterClick.innerHTML = `<p style="color: rgb(130, 213, 112); font-size: 1.3rem;margin-bottom: 1rem;">Seu IMC:</p>`;
+            valorImc.innerHTML = `<pstyle="color: white; font-size: 1rem">O seu IMC é de ${resultado}. Você está com <span style="color:rgb(130, 213, 112);font-weight: bold;" >obesidade grau I. </span>É importante procurar orientação médica para desenvolver um plano de tratamento que possa incluir ajustes na dieta e exercícios físicos.</pstyle=>`;
           } else if (resultado >= 35 && resultado <= 39.9) {
-            valorImc.innerHTML = `<p>O seu IMC é de ${resultado}. Você está com <span>obesidade grau II </span>. É essencial buscar a ajuda de um médico para um plano de tratamento mais intensivo e monitoramento contínuo da saúde.</p>`;
+            afterClick.innerHTML = `<p style="color: rgb(130, 213, 112); font-size: 1.3rem;margin-bottom: 1rem;">Seu IMC:</p>`;
+            valorImc.innerHTML = `<pstyle="color: white; font-size: 1rem">O seu IMC é de ${resultado}. Você está com <span style="color:rgb(130, 213, 112);font-weight: bold;" >obesidade grau II. </span>.É essencial buscar a ajuda de um médico para um plano de tratamento mais intensivo e monitoramento contínuo da saúde.</pstyle=>`;
           } else if (resultado >= 40 && resultado <= 49.9) {
-            valorImc.innerHTML = `<p>O seu IMC é de ${resultado}. Você está com <span>obesidade grau III </span>. É crucial procurar orientação médica urgente para abordar possíveis riscos à saúde e receber suporte adequado.</p>`;
+            afterClick.innerHTML = `<p style="color: rgb(130, 213, 112); font-size: 1.3rem;margin-bottom: 1rem;">Seu IMC:</p>`;
+            valorImc.innerHTML = `<pstyle="color: white; font-size: 1rem">O seu IMC é de ${resultado}. Você está com <span style="color:rgb(130, 213, 112);font-weight: bold;" >obesidade grau III. </span> É crucial procurar orientação médica urgente para abordar possíveis riscos à saúde e receber suporte adequado.</pstyle=>`;
           } else if (resultado >= 50 && resultado <= 59.9) {
-            valorImc.innerHTML = `<p>O seu IMC é de ${resultado}. Você está com <span>obesidade grau IV </span>. É necessário buscar assistência médica imediata para um plano de tratamento abrangente e avaliação dos riscos associados.</p>`;
+            afterClick.innerHTML = `<p style="color: rgb(130, 213, 112); font-size: 1.3rem;margin-bottom: 1rem;">Seu IMC:</p>`;
+            valorImc.innerHTML = `<pstyle="color: white; font-size: 1rem">O seu IMC é de ${resultado}. Você está com <span style="color:rgb(130, 213, 112);font-weight: bold;" >obesidade grau IV. </span> É necessário buscar assistência médica imediata para um plano de tratamento abrangente e avaliação dos riscos associados.</pstyle=>`;
           } else if (resultado >= 60) {
-            valorImc.innerHTML = `<p>O seu IMC é de ${resultado}. Você está com <span>obesidade grau V </span>. Procure assistência médica urgente para tratamento intensivo e avaliação dos riscos graves para sua saúde.</p>`;
+            afterClick.innerHTML = `<p style="color: rgb(130, 213, 112); font-size: 1.3rem;margin-bottom: 1rem;">Seu IMC:</p>`;
+            valorImc.innerHTML = `<pstyle="color: white; font-size: 1rem">O seu IMC é de ${resultado}. Você está com <span style="color:rgb(130, 213, 112);font-weight: bold;" >obesidade grau V. </span>Procure assistência médica urgente para tratamento intensivo e avaliação dos riscos graves para sua saúde.</pstyle=>`;
           } else {
             valorImc.innerHTML = `<p>Não foi possível calcular o IMC. Certifique-se de que todos os valores foram inseridos corretamente e tente novamente.</p>`;
           }
-          headerContent.classList.add("hidden");
-          calculator.classList.add("hidden");
-          result.classList.remove("hidden");
         }
+      } else {
+        alert("Insira todos os campos corretamente!");
       }
     });
   }
-advanceButton();
-backButton();
-getResult();
 
+  getResult();
 });
